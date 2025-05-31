e3bb53fb1e833d9afcacfcdaedea0055570ce939
shivraj <shivrajrk03@gmail.>
2025-05-31 16:18:09 +0530

    new changes

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -9,6 +9,7 @@ import {
   MessageSquare,
   User,
   Phone,
+  Sticker
 } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthImagePattern from "../components/AuthImagePattern";
@@ -62,7 +63,7 @@ const SignUpPage = () => {
             <div className="flex flex-col items-center gap-2 group">
               <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
               group-hover:bg-primary/20 transition-colors">
-                <MessageSquare className="size-6 text-primary" />
+                <Sticker className="size-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free account</p>
--- frontend/src/pages/VerifyEmailPage.jsx
+++ frontend/src/pages/VerifyEmailPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import AuthImagePattern from "../components/AuthImagePattern";
-import { MessageSquare, Loader2 } from "lucide-react";
+import { Loader2, Sticker } from "lucide-react";
 import toast from "react-hot-toast";
 
 const VerifyEmailPage = () => {
@@ -76,7 +76,7 @@ const VerifyEmailPage = () => {
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
-                <MessageSquare className="w-6 h-6 text-primary" />
+                <Sticker className="w-6 h-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Verify Your Email</h1>
               <p className="text-base-content/60">
