05739f1232b874fb5829dcd39e3502a4bd63250f
shivraj <shivrajrk03@gmail.>
2025-06-07 11:57:14 +0530

    4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5001',
-    },
-  },
-})
+});
